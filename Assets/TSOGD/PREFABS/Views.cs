c5d2ac695118eb264a3e4e64329cfff0737989d2
mathausleth <mathieu.hoarau@3wa.io>
2023-01-27 17:47:03 +0100

    Ajout: Mise en place des Views de la LaunchScene. Implementation/Mise a jour: Mise en place de la mecanique des Views.

--- Assets/TSOGD/PREFABS/Views.cs
+++ Assets/TSOGD/PREFABS/Views.cs
@@ -13,6 +13,7 @@ namespace TSOGD.PREFABS { public class Views : MonoBehaviour
     //##### SINGLETON ################################################################################################################
     
     //##### OBJECTS ##################################################################################################################
+    private GameObject _addonsGameObject;
     private ViewsManager _viewsManager;
     private ITransition _viewTransition;
     //##### OBJECTS ARRAYS ###########################################################################################################
@@ -29,7 +30,7 @@ namespace TSOGD.PREFABS { public class Views : MonoBehaviour
     }
     void Update()
     {
-            
+        ViewsMecanism();
     }
     #endregion
 	//################################################################################################################################
@@ -40,10 +41,13 @@ namespace TSOGD.PREFABS { public class Views : MonoBehaviour
     private void InitializeAwakeReferences()
     {
         _viewTransition = GetComponentInChildren<ITransition>();
+        _addonsGameObject = transform.GetChild(1).gameObject;
     }
     private void InitializeStartReferences()
     {
         _viewsManager = ViewsManager.Instance;
+        if (_viewsManager != null) _viewsManager.SetNextView(_nextView);
+        //todo: offrir une solution via la touche echap ?
         if (_viewTransition != null) _viewTransition.Play();
     }
     //################################################################################################################################
@@ -52,8 +56,19 @@ namespace TSOGD.PREFABS { public class Views : MonoBehaviour
     #region MECANIQUE DE LA CLASSE
     //##### PRIMITIVES ###############################################################################################################
     private bool _changeView;
-    public bool ChangeView { get => _changeView; set => _changeView = value; }
+    public bool ChangeView { get => _changeView; set => PlayEndTransition(value); }
+    private bool _waitTransition;
     //################################################################################################################################
+    private void ViewsMecanism(){
+        if (!_addonsGameObject.activeSelf && (bool)_viewTransition?.IsPlayed()) _addonsGameObject.SetActive(true);
+        if (_waitTransition && (bool)_viewTransition?.IsWaitingRestart()) {
+            _waitTransition = false;
+            _changeView = true;
+        }
+    }
+    /// <summary>
+    /// Active le GameObject de la View.
+    /// </summary>
     public void OpenView()
     {
         gameObject.SetActive(true);
@@ -66,7 +81,21 @@ namespace TSOGD.PREFABS { public class Views : MonoBehaviour
     {
         _changeView = false;
         gameObject.SetActive(false);
-    }    
+    }
+    /// <summary>
+    /// Parametre l'attente de la fin de la transition si la valeur de ChangeView passe a [true].
+    /// </summary>
+    /// <param name="playTransition">[bool] joue la transition Close</param>
+    private void PlayEndTransition(bool playTransition) {
+        if (!_waitTransition) {
+            _waitTransition = playTransition;
+            if (_waitTransition && _viewTransition != null) _viewTransition.Stop();
+            else if (_waitTransition && _viewTransition == null) {
+                _waitTransition = false;
+                _changeView = true;
+            }
+        }
+    }
     //################################################################################################################################
     #endregion
 	//################################################################################################################################
