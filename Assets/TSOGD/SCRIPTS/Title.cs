f749c24a320367911a6729a0c1edc28476bc2660
mathausleth <mathieu.hoarau@3wa.io>
2023-01-28 21:18:53 +0100

    Ajout/Implementation: Mise en place de la TitleView. Implementation: Mise en place de la transition de l'Image Clouds.

---
+++ Assets/TSOGD/SCRIPTS/Title.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+namespace TSOGD.SCRIPTS { public class Title : MonoBehaviour
+{
+    //##### SERIALIZE FIELD PARAMETERS ###############################################################################################
+    [SerializeField] private float _cloudsStartPosition = 1240f;
+    [SerializeField] private float _cloudsEndPosition = -415f;
+    [SerializeField] private float _cloudsSpeedTransition = 2f;
+    //##### SERIALIZE FIELD REFERENCES ###############################################################################################
+    [SerializeField] private RectTransform _cloudsRectTransform;
+    //##### SERIALIZE FIELD ARRAYS ###################################################################################################
+    
+    //##### TIMERS ###################################################################################################################
+    
+    //##### SINGLETON ################################################################################################################
+    
+    //##### OBJECTS ##################################################################################################################
+    private Vector3 _initialCloudsPosition;
+    //##### OBJECTS ARRAYS ###########################################################################################################
+    
+    //##### REGIONS ##################################################################################################################
+    #region UNITY API
+    void Awake()
+    {
+        InitializeAwakeReferences();
+    }
+    void Start()
+    {
+        InitializeStartReferences();
+    }
+    void Update()
+    {
+        TitleMecanism();
+    }
+    #endregion
+	//################################################################################################################################
+    #region FONCTIONS D'INITIALISATION
+    //##### PRIMITIVES ###############################################################################################################
+    
+    //################################################################################################################################
+    private void InitializeAwakeReferences()
+    {
+    
+    }
+    private void InitializeStartReferences()
+    {
+        InitializeCloudsPosition();
+    }
+    private void InitializeCloudsPosition()
+    {
+        if (_cloudsRectTransform) {
+            _initialCloudsPosition = _cloudsRectTransform.position;
+            _initialCloudsPosition.x = _cloudsStartPosition;
+        }
+    }
+    //################################################################################################################################
+    #endregion
+	//################################################################################################################################
+    #region MECANIQUE DE LA CLASSE
+    //##### PRIMITIVES ###############################################################################################################
+    
+    //################################################################################################################################
+    private void TitleMecanism() {
+        CloudsTransition();
+    }
+    /// <summary>
+    /// Effectue la transition de l'Image des nuages.
+    /// </summary>
+    private void CloudsTransition() {
+        if (_cloudsRectTransform) {
+            if (_cloudsEndPosition < _cloudsRectTransform.position.x) _cloudsRectTransform.Translate(Vector3.left * Time.deltaTime * _cloudsSpeedTransition);
+            else _cloudsRectTransform.position = _initialCloudsPosition;
+        }
+    }
+    //################################################################################################################################
+    #endregion
+	//################################################################################################################################
+}}
