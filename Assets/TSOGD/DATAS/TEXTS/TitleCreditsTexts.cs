90da60bbd418479e4698c91461146e66a4d54ae2
mathausleth <mathieu.hoarau@3wa.io>
2023-02-09 22:47:22 +0100

    Implementation: Mise en place des Credits. Ajout/Implementation: Mise en place des TitleCreditsTexts. Mise a jour: Remplacement de la couleur des textes par ffb1b9.

---
+++ Assets/TSOGD/DATAS/TEXTS/TitleCreditsTexts.cs
@@ -0,0 +1,23 @@
+namespace TSOGD.DATAS.TEXTS { public static class TitleCreditsTexts
+{
+	//################################################################################################################################
+    #region MECANIQUE DE LA CLASSE
+    //##### PRIMITIVES ###############################################################################################################
+    private static readonly string[] _allTitleCreditsTexts = {
+        "\n\n© 2023 Mathaus Leth.<br><size=18>All Rights Reserved.</size>",
+        "\n\nInspiration à titre d'hommage :<br><size=24><b>«</b>  </size><size=18>The Secret of Monkey Island<br>TM & (c) 1990 LucasArts Entertainment Company.<size=24>  <b>»</b></size></size>",
+        "\n\n<size=24><b>The Secret of Game Developer</b> n'a aucun but commercial.</size>",
+        "\n\n<size=24><b>The Secret of Game Developer</b> est disponible gratuitement.</size>",
+        "\n\n<size=18>Entièrement conçu et développé par :</size><br><b>Mathaus Leth</b>.",
+        "\n\n<size=18>Dans l'idée originale d'évoquer les jeux de mon enfance ...</size>",
+        "\n\n<size=18>... les jeux qui m'ont construit et construit mon imaginaire ...</size>",
+        "\n\n<size=18>... et pour exprimer le métier que j'ai choisi :\ndéveloppeur de jeux vidéos.</size>",
+        "\n\n<size=18>Ainsi, raconter une partie de mon histoire ...</size>"
+    };
+    public static string[] AllTitleCreditsTexts { get => _allTitleCreditsTexts; }
+    //################################################################################################################################
+
+    //################################################################################################################################
+    #endregion
+	//################################################################################################################################
+}}
--- Assets/TSOGD/SCRIPTS/Credits.cs
+++ Assets/TSOGD/SCRIPTS/Credits.cs
@@ -1,18 +1,24 @@
+using TMPro;
+using TSOGD.CONTROLS;
+using TSOGD.DATAS.TEXTS;
+using TSOGD.PREFABS;
 using UnityEngine;
-namespace TSOGD { public class Credits : MonoBehaviour
+namespace TSOGD.SCRIPTS { public class Credits : MonoBehaviour
 {
     //##### SERIALIZE FIELD PARAMETERS ###############################################################################################
-    
+    [SerializeField] private GameObject _creditsTextGameObject;
     //##### SERIALIZE FIELD REFERENCES ###############################################################################################
     
     //##### SERIALIZE FIELD ARRAYS ###################################################################################################
     
     //##### TIMERS ###################################################################################################################
-    
+    private Chrono _creditsTimer;
+    private float _creditsDelay = 4f;
     //##### SINGLETON ################################################################################################################
     
     //##### OBJECTS ##################################################################################################################
-    
+    private TextMeshProUGUI _creditsTextTMPro;
+    private Views _titleView;
     //##### OBJECTS ARRAYS ###########################################################################################################
     
     //##### REGIONS ##################################################################################################################
@@ -33,15 +39,21 @@ namespace TSOGD { public class Credits : MonoBehaviour
 	//################################################################################################################################
     #region FONCTIONS D'INITIALISATION
     //##### PRIMITIVES ###############################################################################################################
-    
+    private int _textIndex;
     //################################################################################################################################
     private void InitializeAwakeReferences()
     {
-    
+        _titleView = gameObject.GetComponentInParent<Views>();
+        _creditsTimer = gameObject.AddComponent<Chrono>();
+        _textIndex = 0;
     }
     private void InitializeStartReferences()
     {
-    
+        if (_creditsTextGameObject) {
+            _creditsTextTMPro = _creditsTextGameObject.GetComponent<TextMeshProUGUI>();
+            _creditsTextGameObject.SetActive(true);
+            SetText();
+        }
     }
     //################################################################################################################################
     #endregion
@@ -50,7 +62,38 @@ namespace TSOGD { public class Credits : MonoBehaviour
     //##### PRIMITIVES ###############################################################################################################
     
     //################################################################################################################################
-    
+    /// <summary>
+    /// Change le texte du TMP.
+    /// </summary>
+    /// <param name="text">[string] nouveau texte</param>
+    private void SetText() {
+        if (_textIndex < TitleCreditsTexts.AllTitleCreditsTexts.Length) {
+            if (_creditsTextTMPro) _creditsTextTMPro.text = TitleCreditsTexts.AllTitleCreditsTexts[_textIndex];
+            if (_creditsTimer) {
+                _creditsTimer.SetEventAction(ReadNextText);
+                _creditsTimer.Wait(_creditsDelay);
+            }
+            _textIndex++;
+        } else {
+            Destroy(_creditsTextTMPro.gameObject);
+            if (_creditsTimer) {
+                _creditsTimer.SetEventAction(GoToIntroView);
+                _creditsTimer.Wait(_creditsDelay+1f);
+            }
+        }
+    }
+    /// <summary>
+    /// Lit tous les textes de la classe static TitleCreditsTexts.
+    /// </summary>
+    private void ReadNextText() {
+        Fixes.BeforeNextText(_creditsTextTMPro, _creditsTimer, SetText);
+    }
+    /// <summary>
+    /// Assigne la valeur ChangeView de la TitleView.
+    /// </summary>
+    private void GoToIntroView() {
+        _titleView.ChangeView = true;
+    }
     //################################################################################################################################
     #endregion
 	//################################################################################################################################
